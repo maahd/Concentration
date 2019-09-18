cbcfdfcc29a8620b03b5d644a8116f661b2d28ea
maahd <maahdsh@gmail.com>
2019-09-18 18:46:08 +0200

    Make Concentration a struct

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -56,10 +56,21 @@ class ViewController: UIViewController
     
     private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randonmIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randonmIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?"
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
     }
 }
 
