31ac7993b72e22f235b38a8f4954e8dde006ebbc
maahd <maahdsh@gmail.com>
2019-09-22 02:18:34 +0200

    Adapt for iPad Preserve state when switching categories

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -16,13 +16,36 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Faces":"😀🥰😄😆😎😏😕😫🤪🥺😃"
     ]
     
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+
+    
     // MARK: - Navigation
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
                 }
             }
         }
